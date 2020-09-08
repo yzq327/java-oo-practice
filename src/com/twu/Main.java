278f70254103b738c24aac10a1c362d80939ba21
Your Name <you@example.com>
2020-09-08 23:10:15 +0800

    test-java-oo

--- src/com/twu/Main.java
+++ src/com/twu/Main.java
@@ -1,7 +1,35 @@
 package com.twu;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+	static Scanner scan=new Scanner(System.in);   
+	static userIndex1 ui=new userIndex1();
+	
     public static void main(String[] args) {
+    	  ui.print();    		
+      
     }
+    
+    public void inputNumber(int userNumber) {
+    	inputName inputn=new inputName();
+    	 if(userNumber==1) {         	        	
+         	//Users user=new Users();	         	
+         	//user.inputname();  
+    		inputn.userInputname();
+         	
+         }else if(userNumber==2) {
+            //Admin admin=new Admin();          	
+         	//admin.inputname();
+        	 inputn.adminInputname();
+         	
+         }else if(userNumber==3){
+         	ui.print();
+         }else {
+         	System.out.println("请从数字1,2,3中选择输入一个");
+         	ui.print();
+         }
+     }
+
+
 }
